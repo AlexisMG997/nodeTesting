76efd31ed9a2d2a9e8999e99e102c4cad66b342e
Magallanes Campos <amagallanes@tijuana.gob.mx>
2023-11-14 08:52:44 -0800

    Dist folder added!

--- index.js
+++ index.js
@@ -96,10 +96,9 @@ app.listen(PORT, () => {
 const express = require('express');
 const cors = require('cors');
 const app = express();
-app.use(cors());
-
-
 app.use(express.json());
+app.use(cors());
+app.use(express.static('dist'));
 
 let notes = [
     {
